a2b6b6737da5b0c36dd5384198d4c4d2b433c0a0
Mitchel Nuñez <u20245202@utp.edu.pe>
2023-07-07 16:30:52 -0500

    :spearkles: crear componente usercomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from "./components/home/home.component";
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardteacherComponent } from './components/dashboardteacher/dashboardteacher.component';
+import { UserComponent } from './components/user/user.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path:'registration',component:RegistrationComponent},
   {path:'login',component:LoginComponent},
   {path:'profesordashboard',component:DashboardteacherComponent},
+  {path:'usuariodashboard',component:UserComponent},
 ];
 
 @NgModule({
