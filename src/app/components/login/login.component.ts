183a23c9924c3dd9a4764fdede3e71f6df182c3c
Mitchel Nuñez <u20245202@utp.edu.pe>
2023-07-14 00:59:53 -0500

    :sparkles: actualización de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { Professor } from 'src/app/models/professor';
+import { LoginService } from 'src/app/services/login.service';
 @Component({
   selector: 'my-app-login',
   templateUrl: './login.component.html',
@@ -7,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user = new User();
+  professor = new Professor();
+  msg = "";
+  adminEmail = "";
+  adminPassword = "";
+  
+  constructor(private _service : LoginService, private _router : Router) { }
 
   ngOnInit(): void {
     $(".admin-login-form").hide();
@@ -53,6 +62,64 @@ export class LoginComponent implements OnInit {
       $(".admin-login-form").hide();
     });
 
+  }
+  loginUser()
+  {
+      this._service.loginUserFromRemote(this.user).subscribe(
+        (data: any) => {
+          console.log(data);
+          console.log("Response Received");
+          sessionStorage.setItem('loggedUser', this.user.email);
+          sessionStorage.setItem('USER', "user");
+          sessionStorage.setItem('ROLE', "user");
+          sessionStorage.setItem('name', this.user.email);
+          sessionStorage.setItem('gender', "male");
+          this._router.navigate(['/userdashboard']);
+        },
+        (error: { error: any; }) => {
+          console.log(error.error);
+          this.msg="Bad credentials, please enter valid credentials !!!";
+        }
+      )
+  }
+
+  loginProfessor()
+  {
+      this._service.loginProfessorFromRemote(this.professor).subscribe(
+        (data: any) => {
+          console.log(data);
+          console.log("Response Received");
+          sessionStorage.clear();
+          sessionStorage.setItem('loggedUser', this.professor.email);
+          sessionStorage.setItem('USER', "professor");
+          sessionStorage.setItem('ROLE', "professor");
+          sessionStorage.setItem('professorname',this.professor.email);
+          sessionStorage.setItem('gender', "male");
+          this._router.navigate(['/professordashboard']);
+        },
+        (error: { error: any; }) => {
+          console.log(error.error);
+          this.msg="Bad credentials, please enter valid credentials !!!";
+        }
+      )
+  }
+
+  adminLogin()
+  {
+    if(this._service.adminLoginFromRemote(this.adminEmail, this.adminPassword)) 
+    {
+      sessionStorage.setItem('loggedUser', this.adminEmail);
+      sessionStorage.setItem('USER', "admin");
+      sessionStorage.setItem('ROLE', "admin");
+      sessionStorage.setItem('name', "admin");
+      sessionStorage.setItem('gender', "male");
+      this._router.navigate(['/admindashboard']);
+    }
+    else 
+    {
+      console.log("Exception Occured");
+      this.msg = 'Bad admin credentials !!!'
+    }
   }
 
 }
