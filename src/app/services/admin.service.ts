0238448f81782f615d9aad873a104ebfa7dc6548
Brandt322 <bgamboaa98@gmail.com>
2023-07-13 23:42:23 -0500

    :sparkles: Agregado servicios para todos los modelos

---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Professor } from '../models/professor';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const NAV_URL = environment.apiURL;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  professor = new Professor();
+
+  constructor(private _http: HttpClient) { }
+
+  public addProfessor(professor: Professor): Observable<any> {
+    return this._http.post<any>(`${NAV_URL}/addProfessor`, professor);
+  }
+
+  getTotalProfessors(): Observable<any[]> | undefined {
+    return this._http.get<any>(`${NAV_URL}/gettotalprofessors`);
+  }
+
+  getTotalUsers(): Observable<any[]> | undefined {
+    return this._http.get<any>(`${NAV_URL}/gettotalusers`);
+  }
+
+  getTotalCourses(): Observable<any[]> | undefined {
+    return this._http.get<any>(`${NAV_URL}/gettotalcourses`);
+  }
+
+  getTotalWishlist(): Observable<any[]> | undefined {
+    return this._http.get<any>(`${NAV_URL}/gettotalwishlist`);
+  }
+
+  getTotalEnrollments(): Observable<any[]> | undefined {
+    return this._http.get<any>(`${NAV_URL}/gettotalenrollments`);
+  }
+
+  getTotalEnrollmentCount(): Observable<any[]> | undefined {
+    return this._http.get<any>(`${NAV_URL}/gettotalenrollmentcount`);
+  }
+
+  getTotalChapters(): Observable<any[]> | undefined {
+    return this._http.get<any>(`${NAV_URL}/gettotalchapters`);
+  }
+
+}
