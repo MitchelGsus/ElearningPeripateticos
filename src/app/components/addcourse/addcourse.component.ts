35f7995db2caff99b08db13e760e4c164effdbf9
Brandt322 <bgamboaa98@gmail.com>
2023-07-18 23:03:02 -0500

    :bug: Solucionado el error de path para añadir curso

--- src/app/components/addcourse/addcourse.component.ts
+++ src/app/components/addcourse/addcourse.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Course } from 'src/app/models/course';
+import { ProfessorService } from 'src/app/services/professor.service';
 
 @Component({
   selector: 'my-app-addcourse',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddcourseComponent implements OnInit {
 
-  constructor() { }
+  course = new Course();
+  msg = ' ';
+
+  constructor(private _professorService: ProfessorService, private _router: Router) { }
 
   ngOnInit(): void {
+    $("#websitelink, #youtubelink").css("display", "none");
+    $("#websitelink").hide();
+
+    $("select").on('change', function () {
+      $(this).find("option:selected").each(function () {
+        var option = $(this).attr("value");
+        if (option === "Website") {
+          $("#websitelink").css("display", "block");
+          $("#youtubelink").css("display", "none");
+        }
+        else if (option === "Youtube") {
+          $("#youtubelink").css("display", "block");
+          $("#websitelink").css("display", "none");
+        }
+      });
+    }).change();
+  }
+
+  addCourse() {
+    this._professorService.addCourse(this.course).subscribe(
+      data => {
+        console.log("Course added Successfully !!!");
+        this._router.navigate(['/addchapter']);
+      },
+      error => {
+        console.log("Process Failed");
+        console.log(error.error);
+        this.msg = "Course with " + this.course.coursename + " already exists !!!";
+      }
+    )
   }
 
 }
